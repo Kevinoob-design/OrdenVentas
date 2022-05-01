68c09f2dd9fa6b19e9de58870005ce20ff4dd637
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2022-04-30 22:38:42 -0400

    Utils and DbAdapter methods.
    Added connection, and generic methods to grab any data from sql database and map it to its corresponding entity.
    
    Added utils class with static method to grab or set any child inside component and find inputs with values corresponding to a tag compared to a list

---
+++ db/DbAdapter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OrdenVentas.db
+{
+    internal static class DbAdapter
+    {
+        private static SqlConnection sqlConnection;
+        private static SqlCommand sqlCommand;
+        private static SqlDataReader dr;
+
+        public static void InitializeConnection()
+        {
+            string Source = "";
+            string InitialCat = "";
+            // string IntSecuirity = "";
+            string UserID = "";
+            string Password = "";
+
+            sqlConnection = new SqlConnection($"{Source} {InitialCat} {UserID} {Password}");
+            sqlConnection.Open();
+        }
+
+        public static List<T> LoadTable<T>(string tableName, string fields = "*", string where = "") where T : new()
+        {
+            sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = $"SELECT {fields} FROM {tableName} {where}";
+
+            dr = sqlCommand.ExecuteReader();
+
+            List<T> listOfItems = new List<T>();
+
+            while (dr.Read())
+            {
+                T item = new T();
+                Type type = item.GetType();
+
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    object boxed = item;
+
+                    type.GetProperty(dr.GetName(i)).SetValue(boxed, dr.GetValue(i));
+
+                    item = (T)boxed;
+                }
+
+                listOfItems.Add(item);
+            }
+            dr.Close();
+
+            return listOfItems;
+        }
+
+        public static List<T> LoadWithSp<T>(Dictionary<string, string> items, string spName) where T : new()
+        {
+            sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = spName;
+
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    sqlCommand.Parameters.AddWithValue($"@{item.Key}", item.Value);
+                }
+            }
+
+            dr = sqlCommand.ExecuteReader();
+
+            List<T> listOfItems = new List<T>();
+
+            while (dr.Read())
+            {
+                T item = new T();
+                Type type = item.GetType();
+
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    object boxed = item;
+
+                    type.GetProperty(dr.GetName(i)).SetValue(boxed, dr.GetValue(i));
+
+                    item = (T)boxed;
+                }
+
+                listOfItems.Add(item);
+            }
+            dr.Close();
+
+            return listOfItems;
+        }
+
+        public static void RunSp(Dictionary<string, string> items, string spName)
+        {
+            sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = spName;
+
+            foreach (var item in items)
+            {
+                sqlCommand.Parameters.AddWithValue($"@{item.Key}", item.Value);
+            }
+
+            sqlCommand.ExecuteNonQuery();
+        }
+    }
+}
---
+++ shared/Utils.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenVentas.shared
+{
+    internal static class Utils
+    {
+        private static Dictionary<string, string> GetCollectionKeyValueFromControlsTags(params Control[] controls)
+        {
+            Dictionary<string, string> collection = new Dictionary<string, string>();
+
+            foreach (var control in controls)
+            {
+                bool isButtonOrLabel = control is Button || control is Label;
+                bool hasTagAndTextValue = control.Tag != "" && control.Text != "";
+
+                if (!isButtonOrLabel && hasTagAndTextValue)
+                {
+                    bool isKeyValueOnCollection = collection.ContainsKey(control.Tag.ToString());
+
+                    if (!isKeyValueOnCollection)
+                    {
+                        collection[control.Tag.ToString()] = control.Text;
+                    }
+                    else
+                    {
+                        collection[control.Tag.ToString()] += " " + control.Text;
+                    }
+                }
+
+                IEnumerable<Control> childControls = control.Controls.OfType<Control>();
+
+                if (childControls != null)
+                {
+                    foreach (var child in childControls)
+                    {
+                        GetCollectionKeyValueFromControlsTags(child);
+                    }
+                }
+            }
+
+            return collection;
+        }
+
+        private static void SetControlsChildWithValueFromCollection<T>(T collection, params Control[] controls)
+        {
+            foreach (var control in controls)
+            {
+                string tag = control.Tag != null ? control.Tag.ToString() : "";
+                bool hasTagAndIsNotEmpty = tag.Length > 0;
+
+                if (hasTagAndIsNotEmpty)
+                {
+                    Type type = typeof(T);
+                    control.Text = type.GetProperty(tag).GetValue(collection).ToString();
+                }
+
+                IEnumerable<Control> childControls = control.Controls.OfType<Control>();
+
+                if (childControls != null)
+                {
+                    foreach (var child in childControls)
+                    {
+                        SetControlsChildWithValueFromCollection(collection, child);
+                    }
+                }
+            }
+        }
+    }
+}
